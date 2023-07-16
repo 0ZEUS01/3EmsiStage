8194d37fd571e45cefccbd04b7a19562b7087706
Mohamed Hakkou <93436273+ImMrHak@users.noreply.github.com>
2023-07-16 19:31:49 +0100

    Add files via upload
    We added a new endpoint for the login and changed the mapping to "/api/....." in the controller

--- Car_Fleet_Management/CarFleet/api/src/main/java/CarFleet/Controller/CarFleetController.java
+++ Car_Fleet_Management/CarFleet/api/src/main/java/CarFleet/Controller/CarFleetController.java
@@ -7,6 +7,7 @@ import CarFleet.Service.CarFleetService;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class CarFleetController {
@@ -15,32 +16,38 @@ public class CarFleetController {
 
 	                                     /* USER ENDPOINTS */
 	
+	// Endpoint to login user
+	@PostMapping("/api/login")
+	public Users loginUser(@RequestBody Map<String, String> requestBody) throws SQLException {
+	    return carFleetService.loginUser(requestBody.get("username_email"), requestBody.get("password"));
+	}
+	
     // Endpoint to get all users
-    @GetMapping("/users")
+    @GetMapping("/api/users")
     public List<Users> getAllUsers() throws SQLException {
         return carFleetService.getAllUsers();
     }
 
     // Endpoint to get a specific user by ID
-    @GetMapping("/users/{id}")
+    @GetMapping("/api/users/{id}")
     public Users getUserById(@PathVariable Long id) throws SQLException {
         return carFleetService.getUserById(id);
     }
 
     // Endpoint to create a new user
-    @PostMapping("/users")
+    @PostMapping("/api/register")
     public Users createUser(@RequestBody Users user) throws SQLException {
         return carFleetService.createUser(user);
     }
 
     // Endpoint to update an existing user
-    @PutMapping("/users/{id}")
+    @PutMapping("/api/users/{id}")
     public Users updateUser(@PathVariable Long id, @RequestBody Users updatedUser) throws SQLException {
         return carFleetService.updateUser(id, updatedUser);
     }
 
     // Endpoint to delete a user
-    @DeleteMapping("/users/{id}")
+    @DeleteMapping("/api/users/{id}")
     public void deleteUser(@PathVariable Long id) throws SQLException {
         carFleetService.deleteUser(id);
     }
@@ -48,31 +55,31 @@ public class CarFleetController {
                                          /* CAR ENDPOINTS */
     
     // Endpoint to get all cars
-    @GetMapping("/cars")
+    @GetMapping("/api/cars")
     public List<Cars> getAllCars() throws SQLException {
         return carFleetService.getAllCars();
     }
 
     // Endpoint to get a specific car by ID
-    @GetMapping("/cars/{id}")
+    @GetMapping("/api/cars/{id}")
     public Cars getCarById(@PathVariable Long id) throws SQLException {
         return carFleetService.getCarById(id);
     }
 
     // Endpoint to create a new car
-    @PostMapping("/cars")
+    @PostMapping("/api/cars")
     public Cars createCar(@RequestBody Cars car) throws SQLException {
         return carFleetService.createCar(car);
     }
 
     // Endpoint to update an existing car
-    @PutMapping("/cars/{id}")
+    @PutMapping("/api/cars/{id}")
     public Cars updateCar(@PathVariable Long id, @RequestBody Cars updatedCar) throws SQLException {
         return carFleetService.updateCar(id, updatedCar);
     }
 
     // Endpoint to delete a car
-    @DeleteMapping("/cars/{id}")
+    @DeleteMapping("/api/cars/{id}")
     public void deleteCar(@PathVariable Long id) throws SQLException {
         carFleetService.deleteCar(id);
     }
@@ -80,32 +87,32 @@ public class CarFleetController {
                                         /* LOCATION ENDPOINTS */
     
     // Endpoint to get all locations
-    @GetMapping("/locations")
-    public List<Locations> getAllLocations() throws SQLException{
+    @GetMapping("/api/locations")
+    public List<Locations> getAllLocations() throws SQLException {
         return carFleetService.getAllLocations();
     }
 
     // Endpoint to get a specific location by ID
-    @GetMapping("/locations/{plate}")
-    public Locations getLocationById(@PathVariable String plate) throws SQLException{
+    @GetMapping("/api/locations/{plate}")
+    public Locations getLocationById(@PathVariable String plate) throws SQLException {
         return carFleetService.getLocationByRegistrationPlate(plate);
     }
 
     // Endpoint to create a new location
-    @PostMapping("/locations")
-    public Locations createLocation(@RequestBody Locations location) throws SQLException{
+    @PostMapping("/api/locations")
+    public Locations createLocation(@RequestBody Locations location) throws SQLException {
         return carFleetService.createLocation(location);
     }
 
     // Endpoint to update an existing location
-    @PutMapping("/locations/{plate}")
-    public Locations updateLocation(@PathVariable String plate, @RequestBody Locations updatedLocation) throws SQLException{
+    @PutMapping("/api/locations/{plate}")
+    public Locations updateLocation(@PathVariable String plate, @RequestBody Locations updatedLocation) throws SQLException {
         return carFleetService.updateLocation(plate, updatedLocation);
     }
 
     // Endpoint to delete a location
-    @DeleteMapping("/locations/{plate}")
-    public void deleteLocation(@PathVariable String plate) throws SQLException{
+    @DeleteMapping("/api/locations/{plate}")
+    public void deleteLocation(@PathVariable String plate) throws SQLException {
         carFleetService.deleteLocation(plate);
     }
 }
